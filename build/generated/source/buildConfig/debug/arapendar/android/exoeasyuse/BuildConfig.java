ae3cc5d37cd0f2e585112a960d05b09b66685740
vefghmhassan <vefghmhassan@gmail.com>
2019-02-26 10:41:06 +0330

    first commit

---
+++ build/generated/source/buildConfig/debug/arapendar/android/exoeasyuse/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package arapendar.android.exoeasyuse;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "arapendar.android.exoeasyuse";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
